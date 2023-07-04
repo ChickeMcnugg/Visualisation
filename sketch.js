4ee9c73b1f60eff5a573739749868402c3cad5da
Chicken <87851518+ChickeMcnugg@users.noreply.github.com>
2023-07-04 15:03:28 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let rotateMultiplier = 360;
-let squareSize = 10;
+let squareSize = 30;
+let offset = 1; //change between 0 and 1 depending on squareSize
 let rotateScale = 0.001;
 let timeScale = 0.00005;
-let img;
+let img = [];
+let tileY = 0;
 
 let x = 0.01, y = 0, z = 0;
 let dx, dy, dz, dt = 0.01;
@@ -10,18 +12,15 @@ let a, b, c, d;
 let pointCount = 100000;
 
 function setup() {
-  createCanvas(min(windowWidth, windowHeight), min(windowWidth, windowHeight));
+  createCanvas(windowWidth, windowHeight);
   background(0);
   angleMode(DEGREES);
-  
-  //Lorentz Attractor
+
+  tileY = int(windowHeight / squareSize) + offset;
+
   a = 10;
   b = 28;
   c = 8 / 3;
-  
-  // //Clifford Attractor
-  // a = 2.07;
-  // b = 1.79;
   
   push();
   translate(width / 2, height / 2);
@@ -32,16 +31,10 @@ function setup() {
   beginShape();
   
   for (let i = 0; i < pointCount; i++) {
-    //Lorentz Attractor
     dx = (a * (y - x)) * dt;
     dy = (x * (b - z) - y) * dt;
     dz = (x * y - c * z) * dt;
-
-    // //Hadley Attractor
-    // dx = (y + a * x * y + x * z) * dt;
-    // dy = (1 - b * pow(x, 2) + y * z) * dt;
-    // dz = (x - pow(x, 2) - pow(y, 2)) * dt;
-    
+  
     x = x + dx;
     y = y + dy;
     z = z + dz;
@@ -52,7 +45,11 @@ function setup() {
   endShape();
   pop();
   
-  img = get();
+  for (let i = 0; i < width; i += squareSize) {
+    for (let j = 0; j < height; j += squareSize) {
+      img.push(get(i, j, min(squareSize, width - i), min(squareSize, height - j)));
+    }
+  }
 }
 
 function draw() {
@@ -64,7 +61,8 @@ function draw() {
       translate(i, j);
       rotate(noise(i * rotateScale, j * rotateScale, millis() * timeScale) * rotateMultiplier);
       scale(0.9, 1.2);
-      image(img.get(i, j, min(squareSize, width - i), min(squareSize, height - j)), 0, 0);
+      image(img[int(i / squareSize) * tileY + int(j / squareSize)],
+            0, 0);
       pop();
     }
   }
